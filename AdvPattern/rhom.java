570c5dd4d8ce2c5c34a83785588c37e63475aea3
slinger <162863023+scriptSlinger41@users.noreply.github.com>
2024-06-23 21:10:40 +0530

    Create rhom.java

---
+++ AdvPattern/rhom.java
@@ -0,0 +1,14 @@
+class pattern{
+public static void main(String []args){
+	int n=5;
+	for(int i=1;i<=5;i++){
+		for(int s=1;s<n-i;s++){
+		System.out.print(" ");
+}
+for(int j=1;j<=5;j++){
+System.out.print("*");
+}
+System.out.println();
+}
+}
+}
