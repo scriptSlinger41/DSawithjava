c58b47d80b8d9612f6be93fb9a4fba06a19326fb
slinger <162863023+scriptSlinger41@users.noreply.github.com>
2024-06-29 13:43:51 +0530

    Create bubble.java

---
+++ Sorting/bubble.java
@@ -0,0 +1,19 @@
+
+class BubbleSort {
+    public static void main(String[] args) {
+        int arr[]={4,6,8,3,1};
+        int temp=0;
+        for(int i=0;i<arr.length-1;i++){
+            for(int j=0;j<arr.length-1-i;j++){
+                if(arr[j]>arr[j+1]){
+                    temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
